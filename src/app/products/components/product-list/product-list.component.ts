3f53224885aeed31815d2ae6ab9ec2a45aee7c29
igor.k <igor.kulishov@adesa.com>
2019-07-06 20:17:50 -0400

    adjusted error messages

--- src/app/products/components/product-list/product-list.component.ts
+++ src/app/products/components/product-list/product-list.component.ts
@@ -8,7 +8,7 @@ import { Router, ActivatedRoute, ParamMap } from '@angular/router';
 import { GetProducts } from '../../store/actions/products.actions';
 import { AppStates } from '../../store/states/app.states';
 import { errorState, Products } from '../../models/products.model';
-import { Observable } from 'rxjs';
+import { Observable, BehaviorSubject } from 'rxjs';
 import { AppCookieService } from '../../../core/services/cookie.service';
 import {AddToCart, CheckOut} from "../../../cart/store/actions/cart.actions";
 import { RemoveProduct } from '../../store/actions/products.actions';
@@ -24,10 +24,12 @@ export class ProductListComponent implements OnInit {
   public products$: Observable<Products[]>;
   public error = false;
   @ViewChild('confirmation_template') confirmation_template: ModalDirective;
-  deleteProductApproveModal: BsModalRef | null;
-  deleteProductConfirmationModal: BsModalRef | null;
+  @ViewChild('error_modal') error_modal: ModalDirective;
+  approveModal: BsModalRef | null;
+  confirmationModal: BsModalRef | null;
+  errorModal: BsModalRef | null;
   deleteProduct: ProductDetails;
-  ifDeleteProductError = false;
+  internalDeleteProductMessage: BehaviorSubject<{state:string;}> = new BehaviorSubject({state:undefined});
 
   constructor(private store: Store<AppStates>,
               private appCookieService: AppCookieService,
@@ -37,10 +39,12 @@ export class ProductListComponent implements OnInit {
 
     this.products$ = this.store.select(
       res => {
-        if(res && res['userLoginReducer'] && res['userLoginReducer']['errorLoading']  && res['userLoginReducer']['errorLoading']['error_message'] == 'remove_product_error') {
-          this.ifDeleteProductError = true;
-          this.deleteProductConfirmationModal.hide();
-          this.deleteProductApproveModal.hide();
+        if(res && res['userLoginReducer'] ) {
+          if(res['userLoginReducer']['errorLoading']  && res['userLoginReducer']['errorLoading']['error_message'] == 'remove_product_error') {
+            this.internalDeleteProductMessage.next({ state: 'delete_product_error' });
+          } else {
+            this.internalDeleteProductMessage.next({ state: 'no_errors' });
+          }
         } if (res && res['productsReducer']) {
           return res['productsReducer'];
         }
@@ -65,14 +69,27 @@ export class ProductListComponent implements OnInit {
         id : this.deleteProduct.id
       }
     ));
-    if(!this.ifDeleteProductError) {
-      this.deleteProductConfirmationModal = this.modalService.show(this.confirmation_template, { class: 'modal-lg' });
-    }
+
+    this.internalDeleteProductMessage.subscribe( (status: any) => {
+        switch (status.state) {
+          case 'delete_product_error':
+            if(this.confirmationModal) { this.confirmationModal.hide()}
+            this.errorModal = this.modalService.show(this.error_modal, { class: 'modal-lg' });
+            break;
+          case 'no_errors':
+            if(this.errorModal) {this.errorModal.hide()}
+            this.confirmationModal = this.modalService.show(this.confirmation_template, { class: 'modal-lg' });
+            break;
+          default:
+        }
+      });
+
+
   }
 
   deleteProductConfirmation(template: TemplateRef<any>, deleteProduct: ProductDetails) {
     this.deleteProduct = deleteProduct;
-    this.deleteProductApproveModal = this.modalService.show(template, { class: 'modal-lg' });
+    this.approveModal = this.modalService.show(template, { class: 'modal-lg' });
   }
 
 }
